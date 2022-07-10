91edc87e9c59778c40bbfc486a1f776a4c52ae04
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-10 15:28:23 +0530

    Welcome to Addessbokk

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+public class Wellcome
+{
+    public Wellcome()
+    {
+        Console.WriteLine("Welcome to Address Book");
+    }
+    public void display()
+    {
+        Console.WriteLine("Welcome to Address Book");
+    }
+    public static void display1(string well)
+    {
+
+        Console.WriteLine(well);
+    }
+    public static void Main()
+    {
+        Wellcome show = new Wellcome();
+        show.display();
+        display1("Welcome to Address Book");
+    }
+}
+
+
